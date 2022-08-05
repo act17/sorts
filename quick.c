2676fd5529c5be7f43ae5f8320fb89e0915cb364
Aiden Taylor <aiden.taylor@protonmail.com>
2022-08-05 00:05:27 -0600

    Updated to Version 1.3.1.

--- quick.c
+++ quick.c
@@ -11,39 +11,39 @@
 /*==============================*/
 
 //This function partitions and sorts the array into two halves.
-int qpart(double * a,int mi,int ma){
-  int pi = a[ma];
-  int in = mi - 1;
-  double tm;
-  for(int lp = mi; lp < ma; lp++){
-    if(a[lp] <= pi){
-      in++;
-      tm = a[lp];
-      a[lp] = a[in];
-      a[in] = tm;
+int qpart(double * array,int min,int max){
+  int pivot = array[max];
+  int index = min - 1;  		//This is the numeric value of the index which the two partitions split.
+  double temp;
+  for(int i = min; i < max; i++){
+    if(array[i] <= pivot){		//This routine places numbers on their correct position.
+      index++;
+      temp = array[i];
+      array[i] = array[index];
+      array[index] = temp;
     }
   }
-  in++;
-  tm = a[ma];
-  a[ma] = a[in];
-  a[in] = tm;
-  return in;
+  index++;
+  temp = array[max];			//This swap is made to correct the index position.
+  array[max] = array[index];
+  array[index] = temp;
+  return index;
 }
 
 //This function serves as a wrapper to qpart();.
-void qksrt(double * a,int mi,int ma){
-  if(mi >= ma || mi < 0)
+void qksrt(double * array,int min,int max){
+  if(min >= max || min < 0)
     return;
-  int pa = qpart(a,mi,ma);
-  qksrt(a,mi,pa-1);
-  qksrt(a,pa+1,ma);
+  int partition = qpart(array,min,max);
+  qksrt(array,min,partition-1);
+  qksrt(array,partition+1,max);
 }
 
 //This function servers as a wrapper to the wrapper qksrt();.
-void quick(double * a,double * b,int mi,int ma){
-  for(int i = 0; i < ma; i++)
-    b[i] = a[i];
-  qksrt(b,mi,ma - 1);
+void quick(double * array,double * fillin,int min,int max){
+  for(int i = 0; i < max; i++)
+    fillin[i] = array[i];
+  qksrt(fillin,min,max - 1);	//Since max is equal to inclusive size, the "max - 1" corrects this.
   return;
 }
 //Massive thanks to Levi Webb (@jarcode-foss on GitHub) for help with pretty much everything.
