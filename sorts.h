7ef7ad953ec307c49d43d383c2a269222250dd41
Aiden Taylor <aiden.taylor@protonmail.com>
2022-08-05 16:48:34 -0600

    Updated to Version 1.3.2.

--- sorts.h
+++ sorts.h
@@ -2,6 +2,6 @@
 #define _SORTS_H
 
 void bubble(double * array,double * sorted,int size);
-void quick(double * array,double * fillin,int min,int max);
+void quick(double * array,double * fillin,int max);
 
 #endif
