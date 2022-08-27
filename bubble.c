1dbdaf84f9f41d783b9111580d141f8356bdbc71
Aiden Taylor <aiden.taylor@protonmail.com>
2022-08-26 20:59:50 -0600

    Redone for a variety of reasons detailed in README.md.

--- bubble.c
+++ bubble.c
@@ -2,40 +2,37 @@
 #include <stdlib.h>
 
 void bubble(double * array,double * sorted,int size){
-	for(int i = 0; i < size; i++)
-		sorted[i] = array[i];
-	int c = 0;					//Number that represents the array entries viewed by the program.
-	double currentnum,nextnum;			//Used as ways to temporarily store array entries.
-	for(int a = 0; a < size; a++){
-		for(int b = 1; b < size - a; b++){	//This for statement runs a routine that moves up the highest value.
 
-			currentnum = sorted[c];		//These are our compared numbers. The value of these operators changes a lot
-			nextnum = sorted[c+1];		//While also being imperitive to the function of the algorithim.
+  //This simply copies our entered array to our to-be-sorted array.
+  for(int i = 0; i < size; i++)
+    sorted[i] = array[i];
 
-			if(currentnum > nextnum){	//Next three lines can be considered the "swap routine".
-				sorted[c] = nextnum;	//This makes the current - soon to be previous - entry the lower value.
-				c++;			//This increases the entry under inspection by one.
-				sorted[c] = currentnum;	//This makes the next - now the current - entry the higher value.
-			}
-			
-			else{				//This routine is the "pass routine".
-				c++;			//It simply passes the "currentnum" value to the next entry, this is done via
-				currentnum = sorted[c];
-			}				//not swapping.
+  int offset = 0;	//This is an offset that the program uses when scanning through entries.
+  double current,next;	//This is temporary storage for numbers when swapping.
 
-		}
+  for(int a = 0; a < size; a++){	//This loop repeats until the array is garunteed to be sorted.
+    
+    for(int b = 1; b < size - a; b++){	//This loop repeats until an entry has reached its garunteed proper place.
 
-		if(a != (size - 1))
-			sorted[c] = currentnum;		//This garuntees that the set-in-stone final value of the entry is the highest.
-							//However, this skips if we're on the last number as to avoid issues.
-		c = 0;					//This resets our offset.
-	}
-	/*for(int i = 0; i < size; i++){
-		printf("\nEntry		Sorted		Original");
-		printf("\n%d		%lf		%lf",i,sorted[i],array[i]);
-	}*/
-	return;
+      current = sorted[offset];
+      next = sorted[offset+1];
+
+      if(current > next){		//This occurs when the two entries must be swapped.
+        sorted[offset] = next;
+	offset++;
+	sorted[offset] = current;
+      }
+
+      else				//This occurs when the opposite is true, and the offest - which is the next highest 
+        offset++;			//number just by the way - is the next entry in the array.
+    }
+    
+    if(a != (size - 1))			//This is an error prevention measure that ensures our final value is what it must be.
+      sorted[offset] = current;
+
+    offset = 0;
+    
+  }
+
+  return;
 }
-
-//Special thanks to Calvin & Hobbes profile picture @jarcode-foss on GitHub for providing me with guidance on the proper usage of
-//'malloc();' alongside the suggestion to convert this into a void and use a pre-allocated array.
